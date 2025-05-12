793af62384e8058cf235fa0b6a713fce5c0bb892
Israel <israel.oliversw@gmail.com>
2025-05-12 16:30:56 -0300

    Adicionando cores de prioridade e um array.

--- script.js
+++ script.js
@@ -14,14 +14,36 @@ function addTask() {
     }
 
     let taskView = document.getElementById("taskView");
-    let newLiElement = document.createElement("li");
-
-    newLiElement.textContent = task.nomeTask + " 🢒 " + task.categoria;
-
-    taskView.appendChild(newLiElement);
+    taskView.textContent = ""
+    
     taskList.push(task);
 
+    taskList.forEach(task => {
+        let newLiElement = document.createElement("li");
+        newLiElement.textContent = task.nomeTask + " 🢒 " + task.categoria;
+        taskView.appendChild(newLiElement);
+
+            if (task.prioridade == "Baixa") {
+        newLiElement.style.backgroundColor = "#beff98";
+        }
+
+        if (task.prioridade == "Média") {
+            newLiElement.style.backgroundColor = "#113242";
+            newLiElement.style.color = "white";
+            newLiElement.style.borderColor = "white";
+        }
+
+        if (task.prioridade == "Alta") {
+            newLiElement.style.backgroundColor = "#6e0b0b";
+        }
+    });
+
+    console.log(task);
     console.log(taskList);
+    nameTask.value = "";
+    category.value = "";
+    priority.value = ""; 
+
 }
 
 buttonTask.addEventListener("click", addTask);
